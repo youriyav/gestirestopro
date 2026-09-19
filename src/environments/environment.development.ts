4f4c3d23edbd2f3d4bd44af9c2895b0acd61d776
yavoucko <yavouckolye@gmail.com>
2026-09-19 14:06:01 +0100

    last commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://gestirestopro.ec-innov.com'
+  apiUrl: 'https://gestirestoproapi.ec-innov.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gestirestopro.ec-innov.com'
+  apiUrl: 'https://gestirestoproapi.ec-innov.com'
 };
